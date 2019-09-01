46edd6572ce5c68e665e065e3fc7edf16cda48a8
Maximov1233 <52001281+Maximov1233@users.noreply.github.com>
2019-09-01 17:36:46 +0600

    consulting+review

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const reviewsTitles = document.querySelectorAll('.reviews-title');
+
+reviewsTitles.forEach((elem) => {
+    
+    elem.addEventListener('click', () => {
+        for (let i = 0; i < reviewsTitles.length; i++) {
+            reviewsTitles[i].classList.remove('li_focus');
+        }
+        if (!elem.classList.contains('li_focus')) {
+            elem.classList.add('li_focus');
+        }
+        
+    });
+});
+
+const reviews = document.querySelectorAll('.review'),
+row = document.querySelector('.reviews .row');
+
+reviewsTitles.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        reviews.forEach((review) => {
+            row.style.display = 'none';
+            review.parentNode.style.display = 'none';
+            if (elem.dataset.review === 'all') {
+                review.parentNode.style.display = '';
+            }
+            else if (elem.dataset.review === review.dataset.review) {
+                review.parentNode.style.display = '';
+            }
+        });
+    });
+    
+});
